a3b95e064e9cd9ea21867e482706f952ec1a4733
Origin1 Technologies <origin1tech@gmail.com>
2015-05-03 19:54:18 -0700

    move config inline

--- config.js
+++ config.js
@@ -9,14 +9,8 @@ System.config({
 System.config({
   "map": {
     "angular": "github:angular/bower-angular@1.3.15",
-    "angular-route": "github:angular/bower-angular-route@1.3.15",
-    "angular-ui-router": "github:angular-ui/ui-router@0.2.14",
-    "angular/router": "github:angular/router@0.5.3",
     "traceur": "github:jmcriffey/bower-traceur@0.0.87",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87",
-    "github:angular/bower-angular-route@1.3.15": {
-      "angular": "github:angular/bower-angular@1.3.15"
-    }
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87"
   }
 });
 
