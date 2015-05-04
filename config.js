fe463dda6463ecf2b64c47c503af9e5aca518782
Origin1 Technologies <origin1tech@gmail.com>
2015-05-03 20:00:23 -0700

    remove pacakges from package.json

--- config.js
+++ config.js
@@ -10,7 +10,13 @@ System.config({
   "map": {
     "angular": "github:angular/bower-angular@1.3.15",
     "traceur": "github:jmcriffey/bower-traceur@0.0.87",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87"
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87",
+    "angular-route": "github:angular/bower-angular-route@1.3.15",
+    "angular-ui-router": "github:angular-ui/ui-router@0.2.14",
+    "angular/router": "github:angular/router@0.5.3",
+    "github:angular/bower-angular-route@1.3.15": {
+      "angular": "github:angular/bower-angular@1.3.15"
+    }
   }
 });
 
