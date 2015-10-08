9a0127dc01c941828c525d7aaf0c1e477279335f
Origin1 Technologies <origin1tech@gmail.com>
2015-10-08 10:08:44 -0700

    remove dup folder in packages.

--- config.js
+++ config.js
@@ -1,22 +1,28 @@
 System.config({
   "baseURL": "/",
+  "defaultJSExtensions": true,
+  "transpiler": "traceur",
   "paths": {
-    "*": "*.js",
-    "github:*": "jspm_packages/github/*.js"
+    "github:*": "jspm_packages/github/*"
   }
 });
 
 System.config({
   "map": {
-    "alexei/sprintf.js": "github:alexei/sprintf.js@1.0.2",
-    "angular": "github:angular/bower-angular@1.3.15",
+    "alexei/sprintf.js": "github:alexei/sprintf.js@1.0.3",
+    "angular": "github:angular/bower-angular@1.4.6",
     "angular-route": "github:angular/bower-angular-route@1.3.15",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.14",
     "angular/router": "github:angular/router@0.5.3",
+    "origin1tech/basap": "github:origin1tech/basap@master",
     "traceur": "github:jmcriffey/bower-traceur@0.0.87",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87",
     "github:angular/bower-angular-route@1.3.15": {
-      "angular": "github:angular/bower-angular@1.3.15"
+      "angular": "github:angular/bower-angular@1.4.6"
+    },
+    "github:origin1tech/basap@master": {
+      "alexei/sprintf.js": "github:alexei/sprintf.js@1.0.3",
+      "angular": "github:angular/bower-angular@1.4.6"
     }
   }
 });
--- jspm_packages/github/alexei/sprintf.js@1.0.2.js
+++
@@ -1,3 +0,0 @@
-define(["github:alexei/sprintf.js@1.0.2/src/sprintf"], function(main) {
-  return main;
-});
